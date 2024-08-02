914d31baf452dd66c842f9c0afb0fcd0df6df996
gaofei <xxoommd@163.com>
2024-08-02 17:25:58 +0800

    update pm2 config

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,11 +2,8 @@ module.exports = {
   apps: [{
     name: "my.video",
     script: "./index.js",
-    env_production: {
-      NODE_ENV: "production"
-    },
-    env_development: {
-      NODE_ENV: "development"
+    "env": {
+      "NODE_ENV": "development"
     }
   }]
 }
